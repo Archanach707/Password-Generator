8a16a720fdecf4c056712139af100a30d4709e99
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-07-05 12:11:35 +0530

    create const variable

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+const resultEl = document.getElementById('result')
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numberEl = document.getElementById('number')
+const symbolsEl = document.getElementById('symbols')
+const generateEl = document.getElementById('generate')
+const clipboardEl = document.getElementById('clipboard')
+
+
 const randomFunc = {
   lower: getRandomLower,
   upper: getRandomUpper,
